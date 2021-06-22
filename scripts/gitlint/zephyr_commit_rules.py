2aec484e0b140adec0ffce5315386d7fb06b9c5b
William Hamshaw <William.Hamshaw@nordicsemi.no>
2021-06-22 15:32:43 +0200

    edit failure message to verify

--- scripts/gitlint/zephyr_commit_rules.py
+++ scripts/gitlint/zephyr_commit_rules.py
@@ -104,7 +104,7 @@ class MaxLineLengthExceptions(LineRule):
     id = "UC4"
     target = CommitMessageBody
     options_spec = [IntOption('line-length', 80, "Max line length")]
-    violation_message = "Line exceeds max length ({0}>{1})"
+    violation_message = "almonds Line exceeds max length ({0}>{1})"
 
     def validate(self, line, _commit):
         max_length = self.options['line-length'].value
